e9af8d530e39b72ab0f4d3aab9f829c33e8f3c33
Lukasz Stefaniak <lustefaniak@gmail.com>
2020-08-10 12:17:24 +0200

    Add some extra test cases

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 if [[ -z "$NAME" ]]; then
     echo "Must provide NAME in environment" 1>&2
@@ -10,4 +10,7 @@ if [[ -z "$VERSION" ]]; then
     VERSION=${FULL_VERSION//v}
 fi
 
-docker run --rm "lustefaniak/docker-gcp-java:${NAME}_${VERSION}" || exit 1
+docker run --rm -e KUBERNETES_MEMORY_LIMIT=1048576000 "lustefaniak/docker-gcp-java:${NAME}_${VERSION}" || exit 1
+docker run --rm -e KUBERNETES_MEMORY_LIMIT=1048576000 -e MIN_NON_HEAP_SIZE_MB=200 "lustefaniak/docker-gcp-java:${NAME}_${VERSION}" || exit 1
+docker run --rm -e KUBERNETES_MEMORY_LIMIT=1048576000 -e HEAP_SIZE_RATIO=40 "lustefaniak/docker-gcp-java:${NAME}_${VERSION}" || exit 1
+docker run --rm -e KUBERNETES_MEMORY_LIMIT=1048576000 -e HEAP_SIZE_RATIO=30 -e MIN_NON_HEAP_SIZE_MB=900 "lustefaniak/docker-gcp-java:${NAME}_${VERSION}" || exit 1
