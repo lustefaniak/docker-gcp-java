7860f5e39d320906c68a15549446376dcce55dcd
Lukasz Stefaniak <lustefaniak@gmail.com>
2020-08-18 22:52:18 +0200

    Make sure image with profiler attached is able to start

--- test.sh
+++ test.sh
@@ -14,3 +14,5 @@ docker run --rm -e KUBERNETES_MEMORY_LIMIT=1048576000 "lustefaniak/docker-gcp-ja
 docker run --rm -e KUBERNETES_MEMORY_LIMIT=1048576000 -e MIN_NON_HEAP_SIZE_MB=200 "lustefaniak/docker-gcp-java:${NAME}_${VERSION}" || exit 1
 docker run --rm -e KUBERNETES_MEMORY_LIMIT=1048576000 -e HEAP_SIZE_RATIO=40 "lustefaniak/docker-gcp-java:${NAME}_${VERSION}" || exit 1
 docker run --rm -e KUBERNETES_MEMORY_LIMIT=1048576000 -e HEAP_SIZE_RATIO=30 -e MIN_NON_HEAP_SIZE_MB=900 "lustefaniak/docker-gcp-java:${NAME}_${VERSION}" || exit 1
+
+docker run --rm -e KUBERNETES_MEMORY_LIMIT=1048576000 -e PROFILER_ENABLE=true "lustefaniak/docker-gcp-java:${NAME}_${VERSION}" || exit 1
